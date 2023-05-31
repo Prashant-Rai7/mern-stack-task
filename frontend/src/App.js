d52d2609e83c61b5a8df83efadbf736dc13e7b50
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-05-31 10:30:39 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import LandingPage from './LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <LandingPage/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
